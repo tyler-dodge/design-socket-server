28ea25c83ff346f7069d6a7a9fafaf0b3796cf5b
Andy Joslin <andytjoslin@gmail.com>
2013-03-07 14:46:07 -0500

    organize, cleanup code

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 design-socket-server
 ====================
+
+* A socket.io server which will serve your html files, and reload CSS whenever any linked stylesheets change.
--- app.js
+++ app.js
@@ -1,83 +1,66 @@
 var express = require('express');
-var app = express(), 
-  http = require('http'), 
-  server = http.createServer(app);
-var io = require('socket.io').listen(server, {
-  'log level': 0
-});
+var http = require('http'); 
+var socket = require('socket.io');
 var events = require('events');
 var util = require('util');
 var fs = require('fs');
-var options = {
-  dir : null,
-  port : "3000"
-};
-var css_files = {
-};
-app.engine('html', function (path, options, fn){
-  var key = path + ':string';
+var path = require('path');
 
-  if ('function' == typeof options) {
-    fn = options, options = {};
-  }
+var default_options = {
+  port: 3000,
+  dir: '.'
+};
+module.exports = function(options) {
+  options = options || {};
+  options.port = options.port || default_options.port;
+  options.dir = options.dir || default_options.dir;
 
-  try {
-    options.filename = path;
-    var str = options.cache ? exports.cache[key] || 
-      (exports.cache[key] = fs.readFileSync(path, 'utf8'))
-    : fs.readFileSync(path, 'utf8');
-    fn(null,str);
-  } catch (err) {
-    fn(err);
-  }
-});
-var RefreshEventer = function(){
-  events.EventEmitter.call(this);
-  this.refresh_file = function(file) {
-    this.emit("refresh", file);
+  var app = express();
+  var server = http.createServer(app);
+  var io = socket.listen(server, {
+    'log level': '0'
+  });
+  var dir = path.resolve(options.dir);
+
+  var RefreshEventer = function(){
+    events.EventEmitter.call(this);
+    this.refresh_file = function(file) {
+      this.emit("refresh", file);
+    };
   };
-};
-util.inherits(RefreshEventer, events.EventEmitter);
-RefreshEventer = new RefreshEventer();
+  util.inherits(RefreshEventer, events.EventEmitter);
+  RefreshEventer = new RefreshEventer();
 
-process.argv.forEach(function (val, index, array) {
-  if (index === 2) {
-    options.dir = val;
-  } else if (index === 3) {
-    options.port = val;
-  }
-});
-if (options.dir === null) {
-  return;
-}
-app.use(function(req, res, next) {
-  if (req.url.match(/\.css/) !== null) {
-    if (req.url.indexOf(".css?") !== -1) {
-      req.url = req.url.substr(0,req.url.indexOf(".css?") + 4);
+  app.use(function(req, res, next) {
+    if (req.url.match(/\.css/) !== null) {
+      if (req.url.indexOf(".css?") !== -1) {
+        req.url = req.url.substr(0,req.url.indexOf(".css?") + 4);
+      }
+      if (!options[req.url]) {
+        options[req.url] = true;
+        fs.watchFile(dir + req.url, { 
+          persistent: true, 
+          interval: 500
+        }, (function(file) {
+          return function() {
+            RefreshEventer.refresh_file(file);
+          };
+        })(req.url));
+      }
+    } else {
     }
-    if (!options[req.url]) {
-      options[req.url] = true;
-      fs.watchFile(options.dir + req.url, { 
-        persistent: true, 
-        interval: 500
-      }, (function(file) {
-        return function() {
-          RefreshEventer.refresh_file(file);
-        };
-      })(req.url));
+    next();
+  });
+  app.set('views', dir );
+  app.use('/design_socket', express['static'](__dirname + "/lib"));
+  
+  var file_cache = {};
+  function handle_html(req, res) {
+    if (req.url.charAt(req.url.length-1) === '/') {
+      req.url += 'index.html';
     }
-  } else {
-  }
-  next();
-});
-app.set('views', options.dir );
-app.use('/design_socket',express['static'](__dirname + "/design_socket"));
-function handle_html(req, res) {
-  if (req.url === '/') {
-    req.url = '/index.html';
-  }
-  res.render(req.url.substr(1), function(err, html) {
-    var matched = html.match(/<head[^>]*>/);
+    var html = file_cache[req.url] || (file_cache[req.url] = fs.readFileSync(dir + req.url));
+    var matched = /<head[^>]*>/.exec(html);
     if (matched !== null) {
       html = html.slice(0,matched.index) +
         '<script src="/socket.io/socket.io.js"></script>' +
@@ -86,19 +69,21 @@ function handle_html(req, res) {
         html.slice(matched.index+matched[0].length);
     }
     res.send(html).status(200);
+  }
+  app.get('/', handle_html);
+  app.all('*.html', handle_html);
+  app.use(express['static'](dir));
+  io.sockets.on('connection', function (socket) {
+    var refresh_event = function(file) {
+      socket.emit("refresh", file);
+    };
+    RefreshEventer.on('refresh', refresh_event);
+    socket.on('disconnect', function() {
+      RefreshEventer.removeListener('refresh',refresh_event);
+    });
   });
-}
-app.all('/', handle_html);
-app.all('*.html', handle_html);
-app.use(express['static'](options.dir));
-io.sockets.on('connection', function (socket) {
-  var refresh_event = function(file) {
-    socket.emit("refresh", file);
-  };
-  RefreshEventer.on('refresh', refresh_event);
-  socket.on('disconnect', function() {
-    RefreshEventer.removeListener('refresh',refresh_event);
-  });
-});
 
-server.listen(+options.port);
+  server.listen(+options.port);
+  console.log("Serving '" + dir + "' at http://localhost:" + options.port + "/");
+};
+
